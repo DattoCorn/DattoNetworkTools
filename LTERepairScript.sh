b557cde8195dde8b1ccfc2e4f2401e314ab7dfb2
DattoCorn <139921759+DattoCorn@users.noreply.github.com>
2023-07-20 12:16:50 -0400

    Update LTERepairScript.sh

--- LTERepairScript.sh
+++ LTERepairScript.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-echo "Datto Networking LTE Tool V1.8"
+echo "Datto Networking LTE Tool V2.0"
 
 # Check the device type
 model=$(cat /etc/datto/model;echo)
-
-# Check the device type
-model=$(cat /etc/datto/model)
 
 if echo "$model" | grep -q "DNA"; then
     modemmanager_cmd="/etc/init.d/dna-modemmanager"
@@ -30,61 +27,63 @@ run_cmd() {
     return $status
 }
 
-# Step 1
-run_cmd "modemstatus --verbose"
+while true; do
+    # Step 1
+    run_cmd "modemstatus --verbose"
 
-# Step 2
-if run_cmd "ping -I lte0 8.8.8.8 -c 3"; then
-    echo "LTE is working. Exiting script."
-    exit 0
-fi
+    # Step 2
+    if run_cmd "ping -I lte0 8.8.8.8 -c 3"; then
+        echo "LTE is working. Exiting script."
+        
+    fi
 
-# Step 3
-run_cmd "lsusb -t"
+    # Step 3
+    run_cmd "lsusb -t"
 
-# Step 4
-run_cmd "modemreboot"
+    # Step 4
+    run_cmd "modemreboot"
 
-# Step 5
-run_cmd "modemreconnect"
+    # Step 5
+    run_cmd "modemreconnect"
 
-# Step 6
-if run_cmd "ping -I lte0 8.8.8.8 -c 3"; then
-    echo "LTE is working after modem reconnect. Exiting script."
-    exit 0
-fi
+    # Step 6
+    if run_cmd "ping -I lte0 8.8.8.8 -c 3"; then
+        echo "LTE is working after modem reconnect. Exiting script."
+        
+    fi
 
-# Step 7
-run_cmd "sequans-gpio-reset"
+    # Step 7
+    run_cmd "sequans-gpio-reset"
 
-# Step 8
-run_cmd "modemreconnect"
+    # Step 8
+    run_cmd "modemreconnect"
 
-# Step 9
-if run_cmd "ping -I lte0 8.8.8.8 -c 3"; then
-    echo "LTE is working after sequans-gpio-reset. Exiting script."
-    exit 0
-fi
+    # Step 9
+    if run_cmd "ping -I lte0 8.8.8.8 -c 3"; then
+        echo "LTE is working after sequans-gpio-reset. Exiting script."
+        
+    fi
 
-# Step 10
-run_cmd "${modemmanager_cmd} stop"
+    # Step 10
+    run_cmd "${modemmanager_cmd} stop"
 
-# Step 11
-echo "Running: pymm"
-pymm &
-sleep 60
-kill $!
-echo "pymm - Completed"
-echo "----------------------------------------------------"
+    # Step 11
+    echo "Running: pymm"
+    pymm &
+    sleep 60
+    kill $!
+    echo "pymm - Completed"
+    echo "----------------------------------------------------"
 
-# Step 12
-run_cmd "${modemmanager_cmd} start"
+    # Step 12
+    run_cmd "${modemmanager_cmd} start"
 
-# Step 13
-if run_cmd "ping -I lte0 8.8.8.8 -c 3"; then
-    echo "LTE is working after dna-modemmanager restart. Exiting script."
-    exit 0
-else
-    echo "LTE still not working. Please check the device."
-    exit 1
-fi
+    # Step 13
+    if run_cmd "ping -I lte0 8.8.8.8 -c 3"; then
+        echo "LTE is working after Pymm -modemmanager restart was accomplished. Exiting script."
+        exit 0
+    else
+        echo "LTE still not working. Restarting the script in 10 seconds. Also please have partner check the antenas of the LTE"
+        sleep 10
+    fi
+done
