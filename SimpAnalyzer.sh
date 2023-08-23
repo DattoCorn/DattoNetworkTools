84167c73aaeb11dbdfe196fedea28a5224c0f3e5
DattoCorn <139921759+DattoCorn@users.noreply.github.com>
2023-08-23 12:08:58 -0400

    Update SimpAnalyzer.sh
    Changed approach on verifying architech of device....

--- SimpAnalyzer.sh
+++ SimpAnalyzer.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
-# Path to the local jq binary in the same directory as the script
-JQ_PATH="./jq-Linux64"
+echo "Simp Analyzer 1.2"
+
+# Check device model
+if cat /etc/datto/model >/dev/null 2>&1; then
+    model=$(cat /etc/datto/model)
+else
+    model="D200"
+fi
+
+# Decide which jq binary to use based on the model
+if [ "$model" = "D200" ]; then
+    JQ_PATH="jq"
+else
+    JQ_PATH="./jq-Linux64"
+fi
 
 # Download the JSON file to /tmp
-curl -k -o /tmp/Vendors.json https://raw.githubusercontent.com/DattoCorn/DattoNetworkTools/main/Vendors.json 
-curl -k -L -o /tmp/jq-Linux64 https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 
+curl -o /tmp/Vendors.json https://raw.githubusercontent.com/DattoCorn/DattoNetworkTools/main/Vendors.json
+curl -L -o /tmp/jq-Linux64 https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 chmod +x /tmp/jq-Linux64
 
 # Retrieve the ARP table
 ARP_TABLE=$(cat /proc/net/arp)
 
 # Print the custom column headers
-printf "----------------------Simp Tool V1.3------------------------\n"
+printf "----------------------Simp Tool V3.2------------------------\n"
 printf "%-15s %-17s %-12s %-15s\n" "IP Address" "HW Address" "Device" "Vendor"
 printf "---------------------------------------------------------\n"
 
@@ -25,8 +38,8 @@ do
   # Extract the first 6 characters of the MAC address and convert to uppercase
   MAC_PREFIX=$(echo "$MAC_ADDRESS" | cut -d ":" -f 1-3 | tr '[:lower:]' '[:upper:]')
   
-  # Search for the MAC prefix in the JSON file and extract the vendor name using the local jq binary
-  VENDOR_NAME=$($JQ_PATH -r --arg MAC_PREFIX "$MAC_PREFIX" '.vendors[] | select((.pattern | ascii_upcase) | contains($MAC_PREFIX | ascii_upcase)) | .name' /tmp/Vendors.json)
+  # Search for the MAC prefix in the JSON file and extract the vendor name using the jq binary
+  VENDOR_NAME=$($JQ_PATH -r --arg MAC_PREFIX "$MAC_PREFIX" '.vendors[] | select(.pattern == $MAC_PREFIX) | .name' /tmp/Vendors.json)
   if [ -z "$VENDOR_NAME" ]; then
     VENDOR_NAME="Unknown"
   fi
